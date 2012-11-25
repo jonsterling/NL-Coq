c7da5b669a778f474141f0254389b54353929ce7
Jonathan Sterling <jonsterling@me.com>
2012-11-25 14:22:00 -0800

    Major Changes! (see below)
    I've changed the argument representation based on a recommendation from
    Chung-chieh Shan to encode the constraint that external arguments should
    follow internal arguments. I've also added a notion of saturated vs.
    unsaturated arguments to avoid conflating conflating the idea of an
    argument not existing with its having been saturated.
    
    I've also pulled apart some of the merging constraints into
    `can_merge_at` and `selects`. Could still be cleaner, but it seems like
    an improvement.
    
    I've reorganized the file into some cleaner modules as well.
    
    Finally, I've provided some shorthands for typing out argument
    structures.

--- BabySteps.v
+++ BabySteps.v
@@ -1,31 +1,34 @@
-Require Import String EquivDec Bool.
+Require Import String EquivDec Bool Datatypes.
 Set Implicit Arguments.
 
-Section CoreTheory.
+Module Utilities.
+  Ltac derive_eq := intros x y; change ({x = y} + {x <> y}); decide equality.
+  Definition option_flatMap {a b : _} (f : a -> option b) (x : option a) :=
+    match x with
+      | Some x => f x
+      | None   => None
+    end.
+End Utilities.
+
+Module CoreTheory.
+  Import Utilities.
 
   (** We'll start with a few syntactic categories. This is of course
   not sufficient to do real syntax, but it will do for now. *)
   Inductive category := D | N | V.
+  Instance cat_eq_eqdec : EqDec category eq. derive_eq. Defined.
 
   (** Each constituent is marked by a set of features; in our current
   theory, features are just a syntactic category, and specifications
   for internal and external arguments. *)
 
-  Inductive features : Set :=
-      { cat : category ;
-        iArg : option features ;
-        eArg : option features
-      }.
+  Inductive arg (rep : Set) : Set :=
+    | sat : arg rep
+    | unsat : rep -> arg rep.
+  Arguments sat [rep].
 
-
-  (** Coq allows us to derive decidable equality for categories. This
-  will prove useful below. *)
-
-  Theorem eq_cat_dec (a : category) (b : category) : {a = b} + {a <> b}.
-  Proof.
-    decide equality.
-  Defined.
-  Program Instance cat_eq_eqdec : EqDec category eq := eq_cat_dec.
+  Definition arguments (rep : Set) := option (arg rep * (option (arg rep))).
+  Inductive features : Set := { cat : category ; args : arguments features }.
 
   (** In our current theory, there are two kinds of arguments:
   internal and external; an internal argument is lower in the tree
@@ -33,10 +36,12 @@ Section CoreTheory.
   selectors for arguments. *)
 
   Inductive position := internal | external.
-  Definition argument_at (p : position) :=
+  Instance pos_eq_eqdec : EqDec position eq. derive_eq. Defined.
+
+  Definition argument_at (p : position) (fs : features) :=
     match p with
-      | internal => iArg
-      | external => eArg
+      | internal => option_map fst (args fs)
+      | external => option_flatMap snd (args fs)
     end.
 
   (** This is where it starts to get interesting. We need a predicate
@@ -50,37 +55,33 @@ Section CoreTheory.
   specification for an argument present. This is because these
   specifications are erased after each merge. *)
 
-  Definition saturated_at (fs : features) (p : position) :=
+  Definition saturated_at (p : position) (fs : features) :=
     match argument_at p fs with
-      | Some _ => false
-      | None   => true
+      | Some sat => true
+      | _ => false
     end.
-
-  Definition fully_saturated (fs : features) :=
-    (saturated_at fs internal) && (saturated_at fs external).
 
   (** First, we determine if a merge is even plausible: for C-merge,
   the internal argument must not already be saturated; for S-merge,
   the internal argument must be saturated, and the external argument
   must not be. *)
 
-  Definition can_merge_at (hfs : features) (p : position) :=
+  Definition saturated_up_to (p : position) (fs : features) :=
     match p with
-      | internal => negb (saturated_at hfs internal)
-      | external => (saturated_at hfs internal) && (negb (saturated_at hfs external))
+      | external => saturated_at internal fs
+      | _ => true
     end.
+
+  Definition can_merge_at (p : position) (fs : features) :=
+    negb (saturated_at p fs) && (saturated_up_to p fs).
 
   (** Now, we can check if the merge will be successful, given the
   syntactic category of each of the participants. *)
 
   Definition selects (p : position) (hfs : features) (fs : features) :=
     match argument_at p hfs with
-      | Some arg =>
-        match cat arg == cat fs with
-          | left  _ => Is_true ((fully_saturated fs) && (can_merge_at hfs p))
-          | right _ => False
-        end
-      | None => False
+      | Some (unsat a) => cat a ==b cat fs
+      | _ => false
     end.
 
   (** Finally, we are ready to model nodes. A node is indexed by its
@@ -95,17 +96,19 @@ Section CoreTheory.
   saturated; if [p] is external, that means that the [eArg] has also
   been saturated. *)
 
+  Definition saturate_at (p : position) (hfs : features) : arguments features :=
+    match p with
+      | external => Some (sat, Some sat)
+      | internal => Some (sat, argument_at external hfs)
+    end.
+
   Inductive node : features -> Set :=
   | head : forall (s : string), forall (fs : features), node fs
   | merge : forall (p : position) (hfs : _) (fs : _)
               (h : node hfs) (n : node fs),
-              selects p hfs fs ->
+              Is_true (selects p hfs fs && can_merge_at p hfs) ->
               node {| cat := cat hfs ;
-                      iArg := None ;
-                      eArg := match p with
-                               | internal => eArg hfs
-                               | external => None
-                             end
+                      args := saturate_at p hfs
                    |}.
 
 
@@ -119,7 +122,8 @@ Section CoreTheory.
 
 End CoreTheory.
 
-Section Examples.
+Module Notations.
+  Import CoreTheory.
 
   (** Let's make some convenient notation for merges. Note that [I] is
   the single constructor for the type [True], and serves as the
@@ -129,54 +133,45 @@ Section Examples.
                                (right associativity, at level 100).
   Notation " spec -| head " := (merge external head spec I)
                                (left associativity, at level 101).
+
+  Definition mkArg (c : category) : arg features := unsat (Build_features c None).
+  Notation " <| i |> " := (Some (mkArg i, None)).
+  Notation " <| i , e |> " := (Some (mkArg i, Some (mkArg e))).
+End Notations.
   
+Module Examples.
+  Import CoreTheory.
+  Import Notations.
+
   (** Let's build up a lexicon. *)
-  Definition dog :=
-    head "dog"
-         {| cat := N ; iArg := None ; eArg := None |}.
-
-  Definition love :=
-    head "love"
-         {| cat  := V ;
-            iArg := Some {| cat := D ; iArg := None ; eArg := None |} ;
-            eArg := Some {| cat := D ; iArg := None ; eArg := None |}
-         |}.
-
- 
-  Definition the :=
-    head "the"
-         {| cat  := D ;
-            iArg := Some {| cat := N ; iArg := None ; eArg := None |} ;
-            eArg := None
-         |}.
-
-  Definition I :=
-    head "I"
-         {| cat := D ; iArg := None ; eArg := None |}.
+  Definition dog := head "dog" {| cat := N ; args := None |}.
+  Definition love := head "love" {| cat  := V ; args := <| D , D |> |}.
+  Definition the := head "the" {| cat  := D ; args := <| N |> |}.
+  Definition fst_prn := head "I" {| cat := D ; args := None |}.
 
   (** We can now build up some phrases. If they type check, then they
   are grammatical within our theory. *)
 
   Definition the_dog := the |- dog.
   Definition love_the_dog := love |- the |- dog.
-  Definition I_love_the_dog := I -| love |- the |- dog.
+  Definition I_love_the_dog := fst_prn -| love |- the |- dog.
 
   (** The last phrase that we constructed represents the following tree:
            V
           / \
          /   \
         /     \
-       I      V         
-             / \        
-            /   \       
-           /     \      
-         love    D      
-                / \     
-               /   \    
-              /     \   
+       I      V
+             / \
+            /   \
+           /     \
+         love    D
+                / \
+               /   \
+              /     \
              the    dog
    *)
-  
+
   (** Evaluating our phrase as a string yields ["I love the dog"]. *)
   Eval simpl in to_string I_love_the_dog.
 
